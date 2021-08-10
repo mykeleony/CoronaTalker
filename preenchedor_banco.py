d27773adb1dcbd191f75895167bee11f347b5e5a
Myke <mykeleony147@gmail.com>
2021-08-10 00:24:13 -0300

    Finalizada a funcionalidade de insercao de perguntas no banco de dados

--- preenchedor_banco.py
+++ preenchedor_banco.py
@@ -10,15 +10,39 @@ conn = sqlite3.connect('q&a_coronavirus.sqlite')
 cur = conn.cursor()
 
 cur.executescript('''
+DROP TABLE IF EXISTS Perguntas_CoronaTalker;
+DROP TABLE IF EXISTS Respostas_CoronaTalker;
+
 CREATE TABLE IF NOT EXISTS Perguntas_CoronaTalker(
     id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    pergunta    TEXT UNIQUE,
-    id_resposta INTEGER
+    pergunta    TEXT UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS Respostas_CoronaTalker(
     id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    resposta    TEXT UNIQUE,
-    id_pergunta INT
+    resposta    TEXT UNIQUE
 )
 ''')
+
+
+#Ignorando erros de certificacao SSL (https):
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+nhtml = urlopen('https://portal.fiocruz.br/coronavirus/perguntas-e-respostas', context = ctx).read()
+html = BeautifulSoup(nhtml, 'html.parser')
+
+perguntas = list()
+
+for linha in html:
+    pergunta = re.findall('/pergunta/.+>(.*\?)', str(linha))
+    perguntas.append(pergunta)
+
+for pergunta in perguntas[3]:
+    cur.execute('INSERT OR IGNORE INTO Perguntas_CoronaTalker (pergunta) VALUES (?)', (pergunta,))
+    cur.execute('SELECT id FROM Perguntas_CoronaTalker WHERE pergunta = ?', (pergunta,))
+    id_pergunta = cur.fetchone()[0]
+    print(id_pergunta, pergunta)
+
+conn.commit()
