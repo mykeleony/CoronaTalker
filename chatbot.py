2fe62c996302523b93e77b6fbdc662182d9a908b
Myke <mykeleony147@gmail.com>
2021-08-10 00:33:33 -0300

    Adição das funções de apresentação das perguntas e do carregamento das respostas

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,13 @@
-# Script de funcionamento do chatbot CoronaTalker
+def mostrar_perguntas():
+    # Funcao que carrega as perguntas disponiveis
+
+def responder(pergunta, nome):
+    # Funcao que, a partir do numero da pergunta, traz sua resposta
+
+
+def start():
+    print('Olá! Meu nome é CoronaTalker. Você pode tirar dúvidas sobre o novo coronavírus comigo!\n')
+    nome = input('Qual o seu nome? \n')
+
+if __name__ == '__main__':
+    start()
