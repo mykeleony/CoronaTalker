c01f182c8c06f8d972bf04ed77d42cc574bdee25
Myke <mykeleony147@gmail.com>
2021-08-10 22:24:18 -0300

    Finalizado o script de preenchimento do banco de dados de perguntas e respostas sobre o novo coronavírus

--- preenchedor_banco.py
+++ preenchedor_banco.py
@@ -20,29 +20,53 @@ CREATE TABLE IF NOT EXISTS Perguntas_CoronaTalker(
 
 CREATE TABLE IF NOT EXISTS Respostas_CoronaTalker(
     id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    resposta    TEXT UNIQUE
+    resposta    TEXT
 )
 ''')
-
 
 #Ignorando erros de certificacao SSL (https):
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-nhtml = urlopen('https://portal.fiocruz.br/coronavirus/perguntas-e-respostas', context = ctx).read()
-html = BeautifulSoup(nhtml, 'html.parser')
+url_principal = 'https://portal.fiocruz.br/coronavirus/perguntas-e-respostas'
 
-perguntas = list()
+n = c = 0
 
-for linha in html:
-    pergunta = re.findall('/pergunta/.+>(.*\?)', str(linha))
-    perguntas.append(pergunta)
+while n < 7:
+    if n <= 0:
+        url_atual = url_principal
 
-for pergunta in perguntas[3]:
-    cur.execute('INSERT OR IGNORE INTO Perguntas_CoronaTalker (pergunta) VALUES (?)', (pergunta,))
-    cur.execute('SELECT id FROM Perguntas_CoronaTalker WHERE pergunta = ?', (pergunta,))
-    id_pergunta = cur.fetchone()[0]
-    print(id_pergunta, pergunta)
+    else:
+        url_atual = url_principal+'?page='+str(n)
+
+    html_atual_feio = urlopen(url_atual, context = ctx).read()
+    html_atual = BeautifulSoup(html_atual_feio, 'html.parser')
+
+    perguntas = re.findall('/pergunta/.+>(.*\?)', str(html_atual))
+
+    for pergunta in perguntas:
+        print(pergunta)
+
+        cur.execute('INSERT OR IGNORE INTO Perguntas_CoronaTalker (pergunta) VALUES (?)', (pergunta,))
+
+    hrefs_respostas = re.findall('<a href="(/pergunta/.*)">', str(html_atual))
+
+    for href_resposta in hrefs_respostas:
+        url_resposta = 'https://portal.fiocruz.br'+href_resposta
+
+        html_resposta_feio = urlopen(url_resposta, context = ctx).read()
+        html_resposta = BeautifulSoup(html_resposta_feio, 'html.parser')
+
+        resposta = re.search('content:.*<p(.*)<', str(html_resposta))[1]
+
+        c += 1
+
+        resposta_formatada = re.sub('<[^<]+?>|dir="ltr"', '', resposta).strip()
+
+        print(c, resposta_formatada)
+        cur.execute('INSERT INTO Respostas_CoronaTalker (resposta) VALUES (?)', (resposta_formatada,))
+
+    n += 1
 
 conn.commit()
