40f81962e50b1b62e998b32b22624cd595e7a3fa
Myke <mykeleony147@gmail.com>
2021-08-13 08:40:45 -0300

    Implementação da condicional de execução do script de preenchimento do banco de dados.

--- app/preenchedor_banco.py
+++ app/preenchedor_banco.py
@@ -15,71 +15,75 @@ import ssl
 
 import re
 
-# Conectando-se (ou criando) ao banco de dados e criando suas tabelas:
-conn = sqlite3.connect('app/q&a_coronavirus.sqlite')
-cur = conn.cursor()
+def start():
+    # Conectando-se (ou criando) ao banco de dados e criando suas tabelas:
+    conn = sqlite3.connect('app/q&a_coronavirus.sqlite')
+    cur = conn.cursor()
 
-cur.executescript('''
-DROP TABLE IF EXISTS Perguntas_CoronaTalker;
-DROP TABLE IF EXISTS Respostas_CoronaTalker;
+    cur.executescript('''
+    DROP TABLE IF EXISTS Perguntas_CoronaTalker;
+    DROP TABLE IF EXISTS Respostas_CoronaTalker;
+    
+    CREATE TABLE Perguntas_CoronaTalker(
+        id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        pergunta    TEXT UNIQUE
+    );
+    
+    CREATE TABLE Respostas_CoronaTalker(
+        id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        resposta    TEXT
+    )
+    ''')
 
-CREATE TABLE Perguntas_CoronaTalker(
-    id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    pergunta    TEXT UNIQUE
-);
+    # Ignorando erros de certificação SSL (https):
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
 
-CREATE TABLE Respostas_CoronaTalker(
-    id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    resposta    TEXT
-)
-''')
+    # Página principal da Fiocruz. O Q&A possui 7 páginas, com 25 perguntas cada.
+    url_principal = 'https://portal.fiocruz.br/coronavirus/perguntas-e-respostas'
 
-# Ignorando erros de certificação SSL (https):
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+    n_pagina = 0
 
-# Página principal da Fiocruz. O Q&A possui 7 páginas, com 25 perguntas cada.
-url_principal = 'https://portal.fiocruz.br/coronavirus/perguntas-e-respostas'
+    while n_pagina < 7:
+        if n_pagina <= 0:
+            url_atual = url_principal       # A página inicial (0) é a página principal do site.
 
-n_pagina = 0
+        else:
+            url_atual = url_principal+'?page='+str(n_pagina)       # As páginas seguintes possuem a sintaxe adicional '?page=n'
 
-while n_pagina < 7:
-    if n_pagina <= 0:
-        url_atual = url_principal       # A página inicial (0) é a página principal do site.
+        html_atual_feio = urlopen(url_atual, context = ctx).read()
+        html_atual = BeautifulSoup(html_atual_feio, 'html.parser') # "Limpando" o código HTML.
 
-    else:
-        url_atual = url_principal+'?page='+str(n_pagina)       # As páginas seguintes possuem a sintaxe adicional '?page=n'
+        # Os textos das perguntas estão posicionados após o fechamento das tags que contêm o href com "/pergunta/":
+        perguntas = re.findall('/pergunta/.+>(.*\?)', str(html_atual))
 
-    html_atual_feio = urlopen(url_atual, context = ctx).read()
-    html_atual = BeautifulSoup(html_atual_feio, 'html.parser') # "Limpando" o código HTML.
+        for pergunta in perguntas:
+            cur.execute('INSERT OR IGNORE INTO Perguntas_CoronaTalker (pergunta) VALUES (?)', (pergunta,))
 
-    # Os textos das perguntas estão posicionados após o fechamento das tags que contêm o href com "/pergunta/":
-    perguntas = re.findall('/pergunta/.+>(.*\?)', str(html_atual))
+        # Os links das páginas das respostas estão no href dessas mesmas tags.
+        hrefs_respostas = re.findall('<a href="(/pergunta/.*)">', str(html_atual))
 
-    for pergunta in perguntas:
-        cur.execute('INSERT OR IGNORE INTO Perguntas_CoronaTalker (pergunta) VALUES (?)', (pergunta,))
+        for href_resposta in hrefs_respostas:
+            url_resposta = 'https://portal.fiocruz.br'+href_resposta
 
-    # Os links das páginas das respostas estão no href dessas mesmas tags.
-    hrefs_respostas = re.findall('<a href="(/pergunta/.*)">', str(html_atual))
+            html_resposta_feio = urlopen(url_resposta, context = ctx).read()
+            html_resposta = BeautifulSoup(html_resposta_feio, 'html.parser')
 
-    for href_resposta in hrefs_respostas:
-        url_resposta = 'https://portal.fiocruz.br'+href_resposta
+            # Os textos das respostas estão contidos na tag <p> das linhas que apresentam o elemento content:(...)
+            resposta = re.search('content:.*<p(.*)<', str(html_resposta))[1]
 
-        html_resposta_feio = urlopen(url_resposta, context = ctx).read()
-        html_resposta = BeautifulSoup(html_resposta_feio, 'html.parser')
+            # Retirando eventuais comandos HTML contidos na resposta extraída e retirando os espaços laterais do texto.
+            resposta_formatada = re.sub('<[^<]+?>|dir="ltr"|>', '', resposta).strip()
 
-        # Os textos das respostas estão contidos na tag <p> das linhas que apresentam o elemento content:(...)
-        resposta = re.search('content:.*<p(.*)<', str(html_resposta))[1]
+            cur.execute('INSERT INTO Respostas_CoronaTalker (resposta) VALUES (?)', (resposta_formatada,))
 
-        # Retirando eventuais comandos HTML contidos na resposta extraída e retirando os espaços laterais do texto.
-        resposta_formatada = re.sub('<[^<]+?>|dir="ltr"|>', '', resposta).strip()
+        n_pagina += 1
 
-        cur.execute('INSERT INTO Respostas_CoronaTalker (resposta) VALUES (?)', (resposta_formatada,))
+    # Atualizando o banco de dados:
+    conn.commit()
 
-    n_pagina += 1
+    cur.close()
 
-# Atualizando o banco de dados:
-conn.commit()
-
-cur.close()
+if __name__ == 'main':
+    start()
