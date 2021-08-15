2ba711cbc5a97b37f0e67cf05467a4214dfdced9
Myke <mykeleony147@gmail.com>
2021-08-15 02:39:03 -0300

    Criação das funções de busca de dados sobre a Covid-19 no Brasil e no mundo.

---
+++ app/dados_covid.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+import re
+
+def dados_covid_mundiais():
+    # Função que retorna os números de casos e mortes pela Covid-19 no mundo.
+
+def dados_covid_brasileiros():
+    # Função que retorna os números de casos e mortes pela Covid-19 no Brasil.
