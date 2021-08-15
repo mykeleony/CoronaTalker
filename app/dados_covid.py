544a6644841413f1fff13bd70d3b5c6f9f27a500
Myke <mykeleony147@gmail.com>
2021-08-15 16:15:13 -0300

    Desenvolvidas as funcionalidades de busca dos números de contaminados e de mortes no Brasil e no mundo.

--- app/dados_covid.py
+++ app/dados_covid.py
@@ -9,22 +9,27 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-def dados_covid_mundiais():
+def buscar_dados_covid():
     # Função que retorna os números de casos e mortes pela Covid-19 no mundo.
 
-    url_mundo = 'https://en.wikipedia.org/wiki/Template:COVID-19_pandemic_data'
+    url_dados = 'https://en.wikipedia.org/wiki/Template:COVID-19_pandemic_data'
 
-    html_covid_mundial = BeautifulSoup(urlopen(url_mundo, context = ctx).read(), 'html.parser')
+    html_dados = BeautifulSoup(urlopen(url_dados, context = ctx).read(), 'html.parser')
 
-    mortes = re.search('<th style.*2px;">(.*)', str(html_covid_mundial))[1]
-    print(mortes)
+    numeros_mundo = re.findall('<th style.*2px;">(.*)', str(html_dados))
+    
+    casos_mundo = numeros_mundo[0]
+    mortes_mundo = numeros_mundo[1]
 
-    for linha in html_covid_mundial:
-        '''print(linha)
-        mortes = re.search('')'''
+    numeros_brasil = re.findall('<td>(.*)', str(html_dados))
 
-def dados_covid_brasileiros():
-    # Função que retorna os números de casos e mortes pela Covid-19 no Brasil.
-    a = 10
+    casos_brasil = numeros_brasil[7]
+    mortes_brasil = numeros_brasil[8]
+    
+    dados_covid = list()
+    dados_covid.extend((casos_mundo, mortes_mundo, casos_brasil, mortes_brasil))
+    
+    return dados_covid
 
-dados_covid_mundiais()
+if __name__ == '__main__':
+    buscar_dados_covid()
