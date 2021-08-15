7d7a3998994a54283d6e76b399c9b2a0cc405b61
Myke <mykeleony147@gmail.com>
2021-08-14 23:46:33 -0300

    Estabelecimento da conexão entre a página inicial e o back-end.

--- CoronaTalkerUI/main.py
+++ CoronaTalkerUI/main.py
@@ -4,11 +4,27 @@ import os
 
 from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
+from PySide2.QtCore import QObject, Slot, Signal
 
+class JanelaPrincipal(QObject):
+    def __init__(self):
+        QObject.__init__(self)
+
+    definirNome = Signal(str)
+
+    @Slot(str)
+    def escrever_boas_vindas(self, nome):
+        self.definirNome.emit('Olá, '+nome+'. Bem vindo(a) ao CoronaTalker.')
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
+
+    # Conectando-se à interface gráfica:
+    main = JanelaPrincipal()
+    engine.rootContext().setContextProperty('backend', main)
+
+    # Carregando o arquivo QML principal:
     engine.load(os.path.join(os.path.dirname(__file__), "qml/main.qml"))
 
     if not engine.rootObjects():
